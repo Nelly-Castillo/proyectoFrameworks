e676e0370f9679f3bfd1e958cd327539aa1cd809
Rusty <sarahimartinez73593@gmail.com>
2024-04-16 09:45:59 -0600

    Primer componente del home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import NavBar from './componets/navBar';
-import './App.css'
+import './App.css';
+import Home from './Home';
 
 function App() {
   return (
-    <>
-      <NavBar></NavBar>
+    <div className=' w-screen h-screen'>
       <div className='bg-teal-600'>
         <h1>Hola, si funciona tailwind</h1>
       </div>
-    </>
+      <Home />
+    </div>
   )
 }
 
