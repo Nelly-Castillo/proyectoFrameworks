bf3f2f17ccd171ba4442c58bac31c30dfedb3aea
Nelly-Castillo <ncastillo08@alumnos.uaq.mx>
2024-05-06 13:27:20 -0600

    Changes Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'AzulOs': '#2A324B',
+          'Azul': '#007FFF',
+          'NaranjaOs': '#B87E14',
+          'Naranja': '#FFA500',
+          'NaranjaTrans50': 'rgba(255,165,0,0.5)',
+          'NaranjaTrans20': 'rgba(255,165,0,0.2)',
+          'VerLima': '#9BC995', 
+          'VerTrans': 'rgba(155,201,149,0.6)',
+          'Blanco': '#FFFFFF',
+          'Negro': '#2c2d2d'
+
+        }
+      },
     },
     plugins: [],
 }
