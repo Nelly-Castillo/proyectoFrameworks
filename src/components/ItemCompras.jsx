2921564f01798b4f8b46ab22e44878c010a15e8a
NELLY CASTILLO CADENAS <99703789+Nelly-Castillo@users.noreply.github.com>
2024-05-24 14:42:27 -0600

    Changesed: the favicon and title

--- src/components/ItemCompras.jsx
+++ src/components/ItemCompras.jsx
@@ -9,26 +9,28 @@ const ItemCompras = () => {
     const closeModal = () => setIsModalOpen(false);
 
     return (
-        <div className="flex flex-col md:flex-row items-center justify-center p-4">
-        <div className="md:w-1/6 p-4">
-            <img 
-            src={foto}
-            alt="Nombre obra" 
-            className="rounded-lg shadow-lg w-full"
-            />
-        </div>
-        <div className="md:w-1/2 p-4">
-            <h2 className="text-xl font-bold mb-2">Nombre obra</h2>
-            <div className="flex justify-between items-center mb-4">
-            <span className="text-lg">Autor</span>
-            <button onClick={openModal} className="text-lg text-black hover:text-Azul">
-                <span className="text-lg">Calificar</span>
-            </button>
+        <div className="flex flex-col md:flex-row items-center justify-around p-3 my-2 shadow-lg rounded-md">
+            <div className="md:w-1/6">
+                <img 
+                src={foto}
+                alt="Nombre obra" 
+                className="rounded-lg w-full"
+                />
             </div>
-            <p className="text-gray-700 mb-4 text-center">Descripción</p>
-            <p className="text-xl font-semibold">$ Precio</p>
-        </div>
-        <ModalCal isOpen={isModalOpen} onClose={closeModal} />
+            <div className="md:w-1/3">
+                <h2 className="text-lg font-bold mb-2">Nombre obra</h2>
+                <div className="flex justify-between items-center mb-4">
+                <span className=" text-sm underline">Autor</span>
+                <button onClick={openModal} className="text-lg text-black hover:text-Azul">
+                    <span className="text-sm">Calificar</span>
+                </button>
+                </div>
+                <p className="text-gray-700 mb-4 text-center text-xs">Descripción</p>
+                <div className='border-t-2 border-NaranjaTrans20'>
+                    <p className="text-lg font-semibold text-center m-1">$ Precio</p>
+                </div>
+            </div>
+            <ModalCal isOpen={isModalOpen} onClose={closeModal} />
         </div>
     );
 };
