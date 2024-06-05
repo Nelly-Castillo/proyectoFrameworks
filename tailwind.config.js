7582d2ab7bee39e062841219f8ddeaf51d334583
Rusty <sarahimartinez73593@gmail.com>
2024-06-05 14:36:39 -0600

    ya jalo again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< dev-rusty
 const { nextui } = require("@nextui-org/react");
-=======
->>>>>>> dev
 
 export default {
     content: [
@@ -38,10 +35,6 @@ export default {
         }
       },
     },
-<<<<<<< dev-rusty
     darkMode: "class",
     plugins: [nextui()],
-=======
-    plugins: [],
->>>>>>> dev
 }
